5cd08b466feb5ec76e94a4d6aa8f901ae83da8e3
wanyos <wanyos@wanyos-PC>
2016-10-15 12:46:38 +0200

    Se incluye la nueva clase ExcepcionPropia

---
+++ ExcepcionPropia.java
@@ -0,0 +1,20 @@
+
+/**
+ * Descripción de la clase
+ * 
+ * @author (Juan José Romero Ramos) 
+ * @version (a version number or a date)
+ */
+public class ExcepcionPropia extends Exception
+{
+    
+
+    public ExcepcionPropia(String msg)
+    {
+         super (msg);
+    }
+
+    
+   
+    
+}
